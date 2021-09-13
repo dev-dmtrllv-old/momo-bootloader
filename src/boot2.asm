61cd184af197cfd04e659aeb6b2373116a397140
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-13 18:45:01 +0200

    removed fill and added unreal mode enable code and GDT

--- src/boot2.asm
+++ src/boot2.asm
@@ -21,6 +21,27 @@ boot_start:
 a20_enabled:
 	mov si, load_msg
 	call print_line
+
+enter_unreal_mode:
+	cli
+	push ds
+
+	lgdt [gdtinfo]
+
+	mov  eax, cr0								; switch to pmode by
+   	or al, 1									; set pmode bit
+   	mov  cr0, eax
+	jmp $ + 2									; tell 386/486 to not crash
+	
+	mov bx, 0x08								; select descriptor 1
+	mov ds, bx									; 8h = 1000b
+
+	and al, 0xFE								; back to realmode
+   	mov cr0, eax								; by toggling bit again
+
+	pop ds
+	sti
+
 	jmp halt
 
 	jmp wait_shutdown
@@ -38,6 +59,22 @@ error_handler a20_failed, a20_fail_msg
 load_msg: 			db "Loading bootloader core...", 0
 a20_fail_msg:		db "Failed to enable the A20 line!", 0
 
-times 510 - ($-$$) db 0
+gdtinfo:
+	dw gdt_end - gdt_desc - 1				; last byte in table
+	dd gdt_desc								; start of table
 
-dw 0xaa55
+gdt_desc:
+	dd 0
+	dd 0
+
+flat_desc:
+	db 0xff
+	db 0xff
+	db 0
+	db 0
+	db 0
+	db 10010010b
+	db 11001111b
+	db 0
+
+gdt_end:
