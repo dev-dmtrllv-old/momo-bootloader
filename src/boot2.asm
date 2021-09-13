a4bc3562fc0db439bb7bfdf0360ed268cc668f8e
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-13 18:56:09 +0200

    set parts in ram as reserved

--- src/boot2.asm
+++ src/boot2.asm
@@ -49,6 +49,27 @@ enter_unreal_mode:
 	jc get_mem_list_err
 	mov word [boot_info_mem_map_size], bp		; store the number of list items
 
+	mov si, boot_info_mem_map
+	mov ax, word [boot_info_mem_map_size]
+	call init_mm								; initialize the memory manager
+
+	mov eax, 0x0								; set memory for IVT and GDT as reserved
+	mov ebx, 0x800
+	call mm_set_reserved
+	jc mm_set_reserved_err
+
+	xor ecx, ecx								; set the bootloader memory as reserved
+	mov cx, [boot_info_mem_map_size]
+	add eax, ecx
+	mov eax, 24
+	mul ecx 
+	add eax, boot_info_mem_map
+	mov ebx, boot_start
+	xchg ebx, eax
+	sub ebx, eax
+	call mm_set_reserved
+	jc mm_set_reserved_err
+
 	jmp halt
 
 	jmp wait_shutdown
@@ -58,17 +79,19 @@ enter_unreal_mode:
 
 error_handler a20_failed, a20_fail_msg
 error_handler get_mem_list_err, mem_list_err_msg
+error_handler mm_set_reserved_err, set_reserved_err_msg
 
 %include "lib/common.asm"
 %include "lib/screen.asm"
 %include "lib/disk.asm"
 %include "lib/a20.asm"
 %include "lib/mem.asm"
+%include "lib/mm.asm"
 
-load_msg: 			db "Loading bootloader core...", 0
-a20_fail_msg:		db "Failed to enable the A20 line!", 0
-mem_list_err_msg:	db "Failed to get the memory list!", 0
-
+load_msg: 				db "Loading bootloader core...", 0
+a20_fail_msg:			db "Failed to enable the A20 line!", 0
+mem_list_err_msg:		db "Failed to get the memory list!", 0
+set_reserved_err_msg:	db "Failed to set memory block as reserved!", 0
 gdtinfo:
 	dw gdt_end - gdt_desc - 1				; last byte in table
 	dd gdt_desc								; start of table
