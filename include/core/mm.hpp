6ffffd53a62f3b2c7bc394cddc374dfa71064043
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-15 23:06:02 +0200

    move struct to implementation file

--- include/core/mm.hpp
+++ include/core/mm.hpp
@@ -4,13 +4,5 @@
 
 namespace mm
 {
-	struct MemBlock
-	{
-		uint64_t base;
-		uint64_t size;
-		uint32_t acpi_flags;
-		uint32_t misc_flags;
-	} __attribute__((packed));
-
-	void init(void * memMap, uint32_t size);
+	void init(void * memMap, uint32_t size, uint32_t coreBinarySize);
 };
