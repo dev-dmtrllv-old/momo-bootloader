b0bf95382dfb57ddac9733504e7991c78de09363
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-16 16:57:47 +0200

    added port io

---
+++ include/core/io.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "core/types.hpp"
+
+namespace IO
+{
+	inline uint8_t inb(uint16_t port)
+	{
+		uint8_t result;
+		__asm__ volatile(
+			"inb %1, %0"
+			: "=a"(result)
+			: "Nd"(port));
+		return result;
+	}
+
+	inline void outb(uint16_t port, uint8_t data)
+	{
+		__asm__ volatile(
+			"outb %0, %1"
+			:
+			: "a"(data), "Nd"(port));
+	}
+
+	inline uint16_t inw(uint16_t port)
+	{
+		uint16_t result;
+		__asm__ volatile(
+			"inw %1, %0"
+			: "=a"(result)
+			: "Nd"(port));
+		return result;
+	}
+
+	inline void outl(uint16_t port, uint16_t data)
+	{
+		__asm__ volatile(
+			"outw %0, %1"
+			:
+			: "a"(data), "Nd"(port));
+	}
+
+	inline uint32_t inl(uint16_t port)
+	{
+		uint32_t result;
+		__asm__ volatile(
+			"inl %1, %0"
+			: "=a"(result)
+			: "Nd"(port));
+		return result;
+	}
+
+	inline void outl(uint16_t port, uint32_t data)
+	{
+		__asm__ volatile(
+			"outl %0, %1"
+			:
+			: "a"(data), "Nd"(port));
+	}
+};
