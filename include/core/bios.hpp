f8ea8690ad9d029ca8a0f627539ef11e84de4e1b
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-19 20:05:30 +0200

    removed bullshit statements...

--- include/core/bios.hpp
+++ include/core/bios.hpp
@@ -4,12 +4,6 @@
 
 #define BIOS_INTERRUPT(interruptNumber, regs) __asm__ volatile(                                                                          \
 	"int " #interruptNumber "\n"                                                                                                         \
-	"mov %%eax, %[a]\n"                                                                                                                  \
-	"mov %%ebx, %[b]\n"                                                                                                                  \
-	"mov %%ecx, %[c]\n"                                                                                                                  \
-	"mov %%edx, %[d]\n"                                                                                                                  \
-	"mov %%edi, %[D]\n"                                                                                                                  \
-	"mov %%esi, %[S]\n"                                                                                                                  \
 	: [a] "=a"((regs).eax), [b] "=b"((regs).ebx), [c] "=c"((regs).ecx), [d] "=d"((regs).edx), [D] "=D"((regs).edi), [S] "=S"((regs).esi) \
 	: "D"((regs).edi), "a"((regs).eax), "b"((regs).ebx), "c"((regs).ecx), "d"((regs).edx), "D"((regs).edi), "S"((regs).esi)              \
 	:)
