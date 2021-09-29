5898a5f1bb9f9ab82567bd2668466d9aa1b748db
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-29 20:47:06 +0200

    update to call vesa and mm init

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,50 +1,21 @@
 #include "core/bios.hpp"
 #include "core/mm.hpp"
+#include "core/vesa.hpp"
 
-void write(const char* str)
-{
-	Bios::Registers regs = {};
-	call_bios_routine(&bios_get_cursor_position, &regs);
-	
-	uint8_t row = Bios::higherReg(regs.dx);
-	uint8_t col = Bios::lowerReg(regs.dx);
+#include "core/string.hpp"
 
-	uint16_t offset = (row * 80) + col;
-	
-	unsigned char* vgaMem = reinterpret_cast<unsigned char*>(0xB8000 + (offset * 2));
-	while (*str != '\0')
-	{
-		if(*str == '\n')
-		{
-			offset = ((offset / 80) + 1) * 80;
-			vgaMem = reinterpret_cast<unsigned char*>(0xB8000 + (offset * 2));
-			*str++;
-		}
-		else
-		{
-			*vgaMem++ = *str++;
-			vgaMem++;
-			offset++;
-		}
-	}
-
-	regs.bx = 0;
-	regs.dx = Bios::combineReg(offset % 80, offset / 80);
-
-	call_bios_routine(&bios_set_cursor_position, &regs);
-}
-
-void writeLine(const char* str)
-{
-	write(str);
-	write("\n");
-}
+#include "core/macros.hpp"
 
 void main()
 {
-	writeLine("Hello, World!");
+	using Color = Vesa::Color;
 
+	Vesa::init();
+	Vesa::clear();
+	
+	INFO("core entry");
+
+	INFO("initializing memory manager...");
+	
 	MM::init();
-
-	writeLine("done");
 }
