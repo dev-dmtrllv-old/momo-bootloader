67c5cd49ad87853ca70213ed5055babbf8e3b361
dmtrllv <dev.dmtrllv@gmail.com>
2021-09-29 17:38:34 +0200

    added simple print macros

--- include/core/macros.hpp
+++ include/core/macros.hpp
@@ -1,4 +1,25 @@
 #pragma once
 
+#include "core/vesa.hpp"
+
 #define PACKED __attribute__((packed))
 #define SECTION(symbol) __attribute__((section(symbol)))
+
+#ifndef INFO_COLOR
+#define INFO_COLOR Vesa::Color::CYAN
+#endif
+#ifndef WARN_COLOR
+#define WARN_COLOR Vesa::Color::YELLOW
+#endif
+#ifndef ERROR_COLOR
+#define ERROR_COLOR Vesa::Color::LIGHT_RED
+#endif
+
+#define INFO(msg) Vesa::write("[INFO] ", INFO_COLOR, Vesa::Color::BLACK);\
+Vesa::writeLine(msg);
+
+#define WARN(msg) Vesa::write("[WARN] ", WARN_COLOR, Vesa::Color::BLACK);\
+Vesa::writeLine(msg);
+
+#define ERROR(msg) Vesa::write("[ERROR] ", ERROR_COLOR, Vesa::Color::BLACK);\
+Vesa::writeLine(msg);
