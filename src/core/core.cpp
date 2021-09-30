9523015b05592bc67342ebb63aa190f8f5be7082
dmtrllv <dev.dmtrllv@gmail.com>
2021-10-01 01:41:12 +0200

    start the shell!

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -5,6 +5,7 @@
 #include "core/macros.hpp"
 #include "core/disk.hpp"
 #include "core/fs.hpp"
+#include "core/shell.hpp"
 
 void main(uint16_t bootDriveNumber)
 {
@@ -18,32 +19,13 @@ void main(uint16_t bootDriveNumber)
 
 	INFO("initializing disk driver...");
 	Disk::init(bootDriveNumber);
-	
+
 	INFO("initializing filesystems...");
 	FS::init();
 
-	FS::PathInfo pi;
+	INFO("initializing shell...");
+	Shell::init();
 
-	const char* path = "/0/momo/boot.cfg"; 
-
-	if(!FS::getPathInfo(&pi, path))
-	{
-		ERROR("OOPS could not get path info!");
-	}
-	else
-	{
-		Vesa::write("Found file ");
-		Vesa::write(path);
-		Vesa::write(" of ");
-		char buf[16];
-		utoa(pi.size, buf, 10);
-		Vesa::write(buf);
-		Vesa::writeLine(" bytes...");
-	
-		void* file = MM::getPage();
-		FS::readFile(path, file);
-		utoa(reinterpret_cast<uint32_t>(file), buf, 16);
-		Vesa::write("File buffer at address: ");
-		Vesa::writeLine(buf);
-	}
+	INFO("starting shell");
+	Shell::start();
 }
